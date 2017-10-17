09ef046c7a61da806182b1d99fc1f8cba5010e5e
chen2013c602 <54longwu@163.com>
2017-10-17 13:05:06 +0800

    First push

---
+++ code/TC_Locomotive.py
@@ -0,0 +1,25 @@
+from thinkbayes import Pmf
+from thinkbayes import Suite
+import matplotlib.pyplot as plt
+class Train(Suite):
+	def __init__(self,hypos):
+		Pmf.__init__(self)
+		for hypo in hypos:
+			self.Set(hypo,1.0/hypo)
+
+	def Likelihood(self,data,hypo):
+		if hypo<data:
+			return 0
+		else :
+			return 1.0/hypo
+
+hypos = xrange(1,1001)
+suite = Train(hypos)
+suite.Update(60)
+L1=[];L2=[]
+for x,y in suite.Items():
+	L1.append(x);L2.append(y)
+plt.plot(L1,L2,)
+plt.axis([0,1000,0,0.02])
+plt.show()
+print suite.Mean()
---
+++ code/TC_Test.py
@@ -0,0 +1,9 @@
+'''from thinkbayes import Pmf
+pmf=Pmf()
+for x in [1,2]:
+	pmf.Set(x,1)
+pmf.Normalize()
+pmf.Mult(1,0.75)
+pmf.Mult(2,0.5)
+for hypo,prob in pmf.Items():
+	print hypo,prob'''
